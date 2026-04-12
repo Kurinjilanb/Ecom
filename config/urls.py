2b901d72423202108eb07f271dd914c2be50ac45
ygg-KB <kb@yougotagift.com>
2026-04-12 21:28:12 +0530

    updated urls

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
+
+def health_check(request):
+    return JsonResponse({'status': 'ok'})
+
+
 urlpatterns = [
+    path('health/', health_check, name='health-check'),
     path('admin/', admin.site.urls),
 
     # API docs
